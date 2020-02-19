75f53384580ca6e6d199873614bd57b143fb3233
Levind Sein <levindsein@gmail.com>
2020-02-19 17:41:32 +0700

    tampat usaha

--- caringin/app/Tempat_usaha.php
+++ caringin/app/Tempat_usaha.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Tempat_usaha extends Model
 {
     protected $table ='tempat_usaha';
-    protected $fillable = ['id_tempat','id_trfkebersihan','id_trfkeamanan','id_trfipk','id_nasabah','kd_kontrol','no_alamat','jml_alamat','nomtr_listrik','nomtr_air','bentuk_usaha'];
+    protected $fillable = ['id_tempat','id_trfkebersihan','id_trfipk','id_trfkeamanan','id_trflistrik','id_trfair','id_nasabah','kd_kontrol','no_alamat','jml_alamat','bentuk_usaha','nomtr_air','nomtr_listrik','updated_at','created_at'];
 }
