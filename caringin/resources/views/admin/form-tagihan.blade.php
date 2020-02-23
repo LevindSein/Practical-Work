cef988110f16a6ffb19d4f9de673d2d4cd2e81ab
Levind Sein <levindsein@gmail.com>
2020-02-23 20:37:32 +0700

    fixing

--- caringin/resources/views/admin/form-tagihan.blade.php
+++ caringin/resources/views/admin/form-tagihan.blade.php
@@ -1,11 +1,11 @@
 @extends('admin.layout')
 @section('content')
-
        <!-- Begin Page Content -->
+  
        <div class="container-fluid">
 
         <!-- Page Heading -->
-        <div class="d-sm-flex align-items-center justify-content-center ">
+        <div class="d-sm-flex align-items-center justify-content-center">
           <h1 class="h3 mb-0 text-gray-800">Tambah Tagihan</h1>
         </div>
 
@@ -13,27 +13,28 @@
           <div class="col-lg-6">
             <div class="p-4">
               <form class="user">
+              @csrf
                 <div class="form-group">
-                  <input type="text" readonly class="form-control form-control-user" id="exampleInputkodeKontrol" placeholder="A-1-001">
+                  Kode Kontrol
+                  <input readonly type="text" name="kode" class="form-control form-control-user" id="exampleInputKode" placeholder="A-1-001">
                 </div>
                 <div class="form-group">
-                  <input type="text" readonly class="form-control form-control-user" id="exampleInputkodeNama" placeholder="Fahni Amsyari">
+                  Nama Nasabah
+                  <input readonly type="text" name="nama" class="form-control form-control-user" id="exampleInputNama" placeholder="Fahni Amsyari">
                 </div>
                 <div class="form-group">
-                  <input type="number" class="form-control form-control-user" id="exampleInputMeterBaruAir" placeholder="Meter baru air">
+                  Meter Baru Air
+                  <input type="number" name="mAir" class="form-control form-control-user" id="exampleInputAir" placeholder="12345">
                 </div>
                 <div class="form-group">
-                  <input type="number" class="form-control form-control-user" id="exampleInputMeterBaruListrik" placeholder="Meter baru listrik">
+                  Meter Baru Listrik
+                  <input type="number" name="mListrik" class="form-control form-control-user" id="exampleInputListrik" placeholder="12345">
                 </div>
-                <a href="index.html" class="btn btn-primary btn-user btn-block">
-                  Tambah
-                </a>
-              </form>
-              
+                <input type="submit" value="Tambah Nasabah" class="btn btn-primary btn-user btn-block">
+              </form>      
             </div>
           </div>
         </div>
 
     <!-- End of Main Content -->
-
 @endsection
